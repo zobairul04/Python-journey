c9ed717fa02c28f4aaa9f8459564361148e02f5b
Abid <zobairul04@gmail.com>
2026-07-14 16:19:58 +0600

    Function

---
+++ Basic/String.py
@@ -0,0 +1,15 @@
+# Different types of string function
+
+str = input("INPUT = ")
+
+print(str[0])  #indexing
+print(str[0:2]) #slicing
+
+print(str.capitalize())
+print(str.count("a"))
+print(str.find("A"))
+print(str.endswith("id"))
+print(str.replace("a","q"))
+
+
+
