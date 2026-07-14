b8b35a5e4020ae792c2c6b8fe9257ccf4cf1ba7e
Abid <zobairul04@gmail.com>
2026-07-14 16:28:20 +0600

    EVEN-ODD

---
+++ Basic/Even-Odd.py
@@ -0,0 +1,15 @@
+# 2 user input , find even odd
+
+a= int(input("ENTER VALUE A= "))
+b= int(input("ENTER VALUE B= "))
+
+if(a%2==0) :
+    print("A= EVEN")
+else :
+    print("B= odd")    
+
+
+if(b%2==0) :
+    print("A= EVEN")
+else :
+    print("B= odd")    
