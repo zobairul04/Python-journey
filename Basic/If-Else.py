c177eed2f95b6037677cefec9c0f786a5dd63400
Abid <zobairul04@gmail.com>
2026-07-14 15:59:17 +0600

    if-else

---
+++ Basic/If-Else.py
@@ -0,0 +1,9 @@
+# user input a , b . if a greater then or equal to b then print true otherwise print false .
+
+a= float(input("VALUE A = "))
+b= float(input("VALUE B = "))
+
+if(a>=b) :
+    print("True")
+else:
+    print("False")    
