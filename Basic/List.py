01b80d77fde23c032211a0ecce439bd4b8f9cba6
Abid <zobairul04@gmail.com>
2026-07-15 11:38:11 +0600

    List Method

---
+++ Basic/List.py
@@ -0,0 +1,26 @@
+# list methods
+
+li= [2.4, 1 , 7]
+
+print(li)
+
+li.append(9)
+print(li)
+
+li.sort()
+print(li)
+
+li.sort(reverse=True)
+print(li)
+
+li.reverse()
+print(li)
+
+li.remove(9)
+print(li)
+
+li.pop(0)
+print(li)
+
+li.insert(0,69)
+print(li)
