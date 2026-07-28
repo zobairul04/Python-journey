897ea49fe73c52279fb6e8a02aa9fcf790a4bffc
Abid <zobairul04@gmail.com>
2026-07-28 21:08:37 +0600

    For loop

--- Basic/Loops.py
+++ Basic/Loops.py
@@ -14,3 +14,31 @@ while i<=3 :
         continue
     print(i)
     i+=1
+
+
+print(" ")
+
+p=[2,4,6,8,9]
+for x in p :
+    print(x)
+
+
+q=[2,4,6,8,9]
+for r in q :
+   pass 
+
+print(" ") 
+
+for g in range(2) :
+    print(g)
+
+print(" ") 
+
+for l in range(2,4) :
+    print(l)
+
+print(" ") 
+
+for r in range(10,2,-2) :
+    print(r)
+   
